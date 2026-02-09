6147e4024bceb4fccfc4f254fc292eac7102040e
Valinee <117422015+Dazuke@users.noreply.github.com>
2026-02-10 06:39:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,6 +47,69 @@ setTimeout(() => {
   document.getElementById("scene-paintball").classList.add("active");
 }, 3500);
 
+/* lovereerrr */
+const points = [
+  // kanan atas
+  { x: 190, y: 60  }, // 1
+  { x: 210, y: 70  }, // 2
+  { x: 230, y: 85  }, // 3
+  { x: 250, y: 105 }, // 4
+  { x: 260, y: 130 }, // 5
+  { x: 250, y: 155 }, // 6
+  { x: 230, y: 175 }, // 7
+  { x: 205, y: 195 }, // 8
+
+  // bawah
+  { x: 180, y: 215 }, // 9
+  { x: 150, y: 230 }, //10
+  { x: 120, y: 215 }, //11
+
+  // kiri bawah
+  { x: 95,  y: 195 }, //12
+  { x: 70,  y: 175 }, //13
+  { x: 50,  y: 155 }, //14
+  { x: 40,  y: 130 }, //15
+  { x: 50,  y: 105 }, //16
+  { x: 70,  y: 85  }, //17
+  { x: 90,  y: 70  }, //18
+  { x: 110, y: 60  }, //19
+
+  // atas tengah (nutup kurva)
+  { x: 130, y: 55  }, //20
+  { x: 150, y: 55  }, //21
+  { x: 170, y: 55  }, //22
+  { x: 160, y: 65  }  //23
+];
+
+function shootPaint(point, index) {
+  const ball = document.createElement("div");
+  ball.className = "ball";
+  ball.style.left = point.x + "px";
+  ball.style.setProperty("--drop", point.y + "px");
+  area.appendChild(ball);
+
+  setTimeout(() => {
+    ball.remove();
+
+    const paint = document.createElement("div");
+    paint.className = "paint";
+    paint.style.left = point.x - 11 + "px";
+    paint.style.top = point.y - 11 + "px";
+
+    area.appendChild(paint);
+
+    // semua pelan-pelan jadi pink
+    setTimeout(() => {
+      paint.classList.add("pink");
+    }, index * 120);
+
+  }, 400);
+}
+
+points.forEach((p, i) => {
+  setTimeout(() => shootPaint(p, i), i * 250);
+});
+
 /* TIMELINE */
 setTimeout(() => {
   showScene(1);
